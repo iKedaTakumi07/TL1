48c2e89f6f7ec229d7629fc723aa2619e31d42b3
ikeda_takumi <zhuomichitisn03@gmail.com>
2026-06-22 12:54:14 +0900

    Update level_editor.py

--- level_editor.py
+++ level_editor.py
@@ -4,6 +4,8 @@ import math
 import gpu
 import gpu_extras.batch
 import copy
+import mathutils
+
 
 # ブレンダーに登録するアドオン情報
 bl_info = { 
@@ -89,6 +91,15 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
         #カスタムプロパティ'file_name'
         if "file_name" in object:
             self.write_and_print(file, indent + "N %s" % object["file_name"])
+        #カスタムプロパティ'collision'
+        if "collider" in object:
+            self.write_and_print(file, indent + "C %s" % object["collider"])
+            temp_str = indent + "CC %f %f %f"
+            temp_str %= (object["collider_center"][0],object["collider_center"][1],object["collider_center"][2])
+            self.write_and_print(file,temp_str)
+            temp_str = indent + "CS %f %f %f"
+            temp_str %= (object["collider_size"][0],object["collider_size"][1],object["collider_size"][2])
+            self.write_and_print(file,temp_str)
         self.write_and_print(file, indent +'END')
         self.write_and_print(file, '')
 
@@ -162,6 +173,23 @@ class DrawCollider:
 
         # 現在シーンのオブジェクトリストを走査
         for object in bpy.context.scene.objects:
+
+            #コライダープロパティがなければ、描画をスキップ
+            if not "collider" in object:
+                continue
+
+            #中心点、サイズの変数を宣言
+            center = mathutils.Vector((0,0,0))
+            size = mathutils.Vector((2,2,2))
+
+            #プロパティから値を取得
+            center[0] = object["collider_center"][0]
+            center[1] = object["collider_center"][1]
+            center[2] = object["collider_center"][2]
+            size[0] = object["collider_size"][0]
+            size[1] = object["collider_size"][1]
+            size[2] = object["collider_size"][2]
+
             # 追加前の頂点数
             start = len(vertices["pos"])
 
@@ -169,11 +197,14 @@ class DrawCollider:
             for offset in offsets:
                 #オブジェクトの中心座標をコピーw!
                 
-                pos = copy.copy(object.location)
+                pos = copy.copy(center)
                 #中心点を基準に拡張店ごとにずらす
                 pos[0] += offset[0] * size[0]
                 pos[1] += offset[1] * size[1]
                 pos[2] += offset[2] * size[2]
+                #ローカル座標からワールド座標に変換
+                pos = object.matrix_world @ pos
+
                 #頂点データリストに座標を追加
                 vertices["pos"].append(pos)
 
@@ -206,6 +237,42 @@ class DrawCollider:
         # 描画
         batch.draw(shader)
 
+#オペレータ カスタムプロパティ['collider']追加
+class MYADDOM_OT_add_collider(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_add_collider"
+    bl_label = "コライダー 追加"
+    bl_description = "['collider']カスタムプロパティを追加します"
+    bl_optopns = {"REGISTER", "UNDO"}
+
+    def execute(self,context):
+
+        #['collider']カスタムプロパティを追加
+        context.object["collider"] = "BOX"
+        context.object["collider_center"] = mathutils.Vector((0,0,0))
+        context.object["collider_size"] = mathutils.Vector((2,2,2))
+
+        return{"FINISHED"}
+    
+#パネル コライダー
+class OBJECT_PT_collider(bpy.types.Panel):
+    bl_iname = "OBJECT_PT_collider"
+    bl_label = "Cpllider"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+
+    # サブメニューの描画
+    def draw(self,context):
+
+        #パネルに項目を追加
+        if "collider" in context.object:
+            #すでにぷろぱちぃがあれば,ぷろぱちぃを表示
+            self.layout.prop(context.object, '["collider"]',text = "Type")
+            self.layout.prop(context.object, '["collider_center"]',text = "Center")
+            self.layout.prop(context.object, '["collider_size"]',text = "Size")
+        else:
+             #プロパティがなければ、ぷろぱちぃ追加ボタンを表示
+            self.layout.operator(MYADDOM_OT_add_collider.bl_idname)
 
 #パネル ファイル名
 class OBJECT_PT_file_name(bpy.types.Panel):
@@ -275,6 +342,13 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
 
         #区切り線
         self.layout.separator()
+        
+        #トップバーの「エディタメニュー」に項目(オペレータ)を追加
+      
+
+
+        #区切り線
+        self.layout.separator()
 
         #トップバーの「エディタメニュー」に項目(オペレーター)を追加
         self.layout.operator("wm.url_open_preset", text = "Manual", icon = "HELP")
@@ -293,6 +367,8 @@ classes =(
     TOPBAR_MT_my_menu,
     MYADDON_OT_add_filename,
     OBJECT_PT_file_name,
+    MYADDOM_OT_add_collider,
+    OBJECT_PT_collider,
 )
 
 # アドオン有効化時コールバック
